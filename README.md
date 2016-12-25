0c480ca86e5a7d641c233933b0109b457c272d83
Steve Jorgensen <stevej@stevej.name>
2016-12-25 04:24:25 -0800

    Render wide & narrow 1/2-diamond triangles

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Renders Penrose Tilings in a Web browser
 
 This project is a work in progress and does not do anything yet.
+
+Working from info at http://preshing.com/20110831/penrose-tiling-explained/
--- penrose.js
+++ penrose.js
@@ -1,9 +1,17 @@
 (function(){
-  var a18 = 18 * Math.PI / 180
-    , sin18 = Math.sin(a18)
-    , narrowDiamondMarkup =
-        "<path d='M -1 0 L 0 " + sin18 + " L 1 0 L 0 -" + sin18 + " z' fill='orange' vector-effect='non-scaling-stroke' stroke='black' />"
-    , tilingEl = document.getElementById('penrose-tiling')
+  var a36 = 36 * Math.PI / 180
+    , sin36 = Math.sin(a36)
+    , cos36 = Math.cos(a36)
+    , a72 = 72 * Math.PI / 180
+    , sin72 = Math.sin(a72)
+    , cos72 = Math.cos(a72)
+    , narrowTriangleMarkup =
+        "<path d='M 1 0 L 0 0 L " + cos36 + " " + sin36 + "' fill='orange' vector-effect='non-scaling-stroke' stroke='black' />"
+    , wideTriangleMarkup =
+        "<path d='M 1 0 L 0 0 L " + cos72 + " " + sin72 + "' fill='lightblue' vector-effect='non-scaling-stroke' stroke='black' />"
+    , tilingEl = document.getElementById('pt-reference-frame')
   ;
-  tilingEl.innerHTML = narrowDiamondMarkup;
+  tilingEl.innerHTML =
+    "<g transform='translate(-0.9 -0.5)'>" + wideTriangleMarkup + "</g>" +
+    "<g transform='translate(-0.1 0.1)'>" + narrowTriangleMarkup + "</g>";
 })();
