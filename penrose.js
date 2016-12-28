4b0c1e820d5e49f6e5ba4aaf224350f7355a5a7d
Steve Jorgensen <stevej@stevej.name>
2016-12-28 05:16:40 -0800

    Start new vertex-centric approach

--- penrose.js
+++ penrose.js
@@ -1,70 +1,60 @@
 (function(document, Math){
-  var a36 = 36 * Math.PI / 180
-    , sin36 = Math.sin(a36)
-    , cos36 = Math.cos(a36)
-    , a108 = 108 * Math.PI / 180
-    , sin108 = Math.sin(a108)
-    , cos108 = Math.cos(a108)
-    , narrowTriangleMarkup =
-        "<path" +
-            " id='narrow-triangle'" +
-            " d='" +
-                 "M 1 0" +
-                " L 0 0" +
-                " L " + cos36 + " " + sin36 +
+  "use strict";
 
-             "'" +
-            " fill='orange'" +
-            " vector-effect='non-scaling-stroke'" +
-            " stroke='black'" +
-        " />"
-    , wideTriangleMarkup =
-        "<path" +
-            " id='wide-triangle'" +
-            " d='" +
-                 "M 1 0" +
-                " L 0 0" +
-                " L " + cos108 + " " + sin108 +
-             "'" +
-            " fill='lightblue'" +
-            " vector-effect='non-scaling-stroke'" +
-            " stroke='black'" +
-        " />"
-    , defsEl = document.getElementById('pt-defs')
-    , tilingEl = document.getElementById('pt-reference-frame')
+  var windowEl = document.getElementById('pt-window')
+    , tilingEl = document.getElementById('pt-tiling')
+    , fatTileEl = document.getElementById('fat-tile')
+    , skinnyTileEl = document.getElementById('skinny-tile')
+    , fatPSL = fatTileEl.pathSegList
+    , skinnyPSL = skinnyTileEl.pathSegList
+    , a72 = degToRad(72)
+    , cos72 = Math.cos(a72)
+    , sin72 = Math.sin(a72)
+    , a36 = degToRad(36)
+    , cos36 = Math.cos(a36)
+    , sin36 = Math.sin(a36)
   ;
 
-  defsEl.innerHTML = narrowTriangleMarkup + wideTriangleMarkup;
+  fatPSL.appendItem( fatTileEl.createSVGPathSegMovetoAbs(0.0, 0.0) );
+  fatPSL.appendItem( fatTileEl.createSVGPathSegLinetoAbs(1.0, 0.0) );
+  fatPSL.appendItem( fatTileEl.createSVGPathSegLinetoAbs(1.0 + cos72, sin72) );
+  fatPSL.appendItem( fatTileEl.createSVGPathSegLinetoAbs(cos72, sin72) );
+  fatPSL.appendItem( fatTileEl.createSVGPathSegClosePath() );
 
-  var initialShapeSelectEl =
-          document.getElementById('initial-shape-select');
+  skinnyPSL.appendItem( skinnyTileEl.createSVGPathSegMovetoAbs(0.0, 0.0) );
+  skinnyPSL.appendItem( skinnyTileEl.createSVGPathSegLinetoAbs(1.0, 0.0) );
+  skinnyPSL.appendItem( skinnyTileEl.createSVGPathSegLinetoAbs(1.0 + cos36, sin36) );
+  skinnyPSL.appendItem( skinnyTileEl.createSVGPathSegLinetoAbs(cos36, sin36) );
+  skinnyPSL.appendItem( skinnyTileEl.createSVGPathSegClosePath() );
 
-  function renderTiling() {
-    var initialShapeSelectEl = document.getElementById('initial-shape-select');
-    switch(initialShapeSelectEl.value) {
-      case "wt":
-        tilingEl.innerHTML =
-          "<use href='#wide-triangle' xlink:href='#wide-triangle' />";
-        break;
-      case 'nt':
-        tilingEl.innerHTML =
-          "<use href='#narrow-triangle' xlink:href='#narrow-triangle' />";
-        break;
-      case 'wd':
-        tilingEl.innerHTML =
-          "<use href='#wide-triangle' xlink:href='#wide-triangle' />" +
-          "<use href='#wide-triangle' xlink:href='#wide-triangle' transform='scale(-1 -1) translate(" + (-(cos108 + 1.0)) + " " + (-sin108) + ")'/>";
-        break;
-      case 'nd':
-        tilingEl.innerHTML =
-          "<use href='#narrow-triangle' xlink:href='#narrow-triangle' />" +
-          "<use href='#narrow-triangle' xlink:href='#narrow-triangle' transform='scale(-1 -1) translate(" + (-(cos36 + 1.0)) + " " + (-sin36) + ")'/>";
-        break;
-    }
+  renderTiles();
+  adjustWindow();
+
+  function renderTiles() {
+    tilingEl.innerHTML =
+      "<use href='#fat-tile' xlink:href='#fat-tile' />" +
+      "<use href='#skinny-tile' xlink:href='#skinny-tile' transform='rotate(72)' />";
   }
 
-  renderTiling();
+  function adjustWindow() {
+    var extents
+      , contentSize
+    ;
 
-  initialShapeSelectEl.addEventListener('change', renderTiling);
+    extents = tilingEl.getBBox();
+    contentSize = extents.width > extents.height ?
+      extents.width :
+      extents.height;
+    contentSize += 2.0;
+    windowEl.transform.baseVal.getItem(0).setScale(2.0 / contentSize, 2.0 / contentSize);
+    windowEl.transform.baseVal.getItem(1).setTranslate(-extents.x - extents.width / 2.0, -extents.y - extents.height / 2.0);
+  }
+
+  function degToRad(degrees) {
+    degrees = degrees % 360;
+    if(degrees > 180) { degrees = 360 - degrees; }
+    if(degrees < -180) { degrees = 360 + degrees; }
+    return degrees * Math.PI / 180;
+  }
 
 })(document, Math);
